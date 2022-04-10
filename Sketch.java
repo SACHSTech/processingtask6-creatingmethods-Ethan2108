83b75141c9f90668e2c53540bf518ac2fffbb7cb
Ethan2108 <ethan.au23@ycdsbk12.ca>
2022-04-10 01:12:04 -0400

    grass and sun finished

--- Sketch.java
+++ Sketch.java
@@ -17,45 +17,60 @@ public class Sketch extends PApplet {
    */
   public void setup() {
     background(210, 255, 173);
+
+    // Return statement to output center of the house
+    System.out.println("Center of the house 1 (x): " + returnOutput(25, 50, 400));
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	
-    // Sun
-    fill(246, 255, 0);
-    ellipse(460, 50, 150, 150);
 
-    // House
-    fill(0, 0, 0);
-    rect(120, 240, 220, 220);
+    // Grass Rectangle
+    grass(0, 300, 400, 100);
+
+    // Sun 
+    sun(50, 60, 80);
+
+    // House 1
+    house(200, 100, 200);
+
+    // House 2 
+    house(50, intReturnValue(100, 200), 100);
     
-    // Window 1
-    fill(222, 222, 222);
-    rect(130, 290, 50, 50);
+  }
+  /**
+  * Draws a base rectangle for the grass on the drawing
+  * 
+  * @param intGrassX x-coordinate for the grass
+  * @param intGrassY y-coordinate for the grass
+  * @param intGrassWidth width of grass
+  * @param intGrassHeight height of grass
+  * 
+  */
+  private void grass(int intGrassX, int intGrassY, int intGrassWidth, int intGrassHeight)
+  { 
+    // Draw the rectangle base for the grass
+    stroke(0, 120, 0);
+    fill(0, 120, 0);
+    rect(intGrassX, intGrassY, intGrassWidth, intGrassHeight);
+  }
 
-    // Window 2
-    fill(222, 222, 222);
-    rect(200, 290, 50, 50);
-
-    // Window 3
-    fill(222, 222, 222);
-    rect(270, 290, 50, 50);
-
-    // Grass
-    fill(8, 242, 0);
-    rect(0, 450, 500, 80);
-
-    // Pathway
-    fill(216, 227, 148);
-    rect(200, 450, 70, 80);
-
-    // Roof
-    fill(44, 101, 156);
-    triangle(80, 240, 250, 140, 380, 240);
+   /**
+  * Draws the circle 
+  * 
+  * @param intSunX x-coordinate of the sun
+  * @param intSunY y-coordinate of the sun
+  * @param intSunSize size of the sun
+  * 
+  */
+  private void sun(int intSunX, int intSunY, int intSunSize)
+  { 
+    // Draw sun
+    stroke(255, 167, 0);
+    fill(255, 255, 0);
+    ellipse(intSunX, intSunY, intSunSize, intSunSize);
   
   // define other methods down here.
 }
